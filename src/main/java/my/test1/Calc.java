fb0b74fd58fc508b3d81a0a676d7853882cbb3d6
Квасов Сергей Сергеевич <sskvasov@dev.vtb.ru>
2021-08-12 14:10:46 +0300

    первый

--- src/main/java/my/test1/Calc.java
+++ src/main/java/my/test1/Calc.java
@@ -1,10 +1,14 @@
 package my.test1;
 
-public class Calc {
+import my.test2.Calcable;
+
+public class Calc implements Calcable {
+    @Override
     public int plus(int x, int y) {
         return x + y;
     }
 
+    @Override
     public int minus(int x, int y) {
         return x - y;
     }
